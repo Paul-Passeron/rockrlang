3b3832c944b607111ac8a4936104f53e7354f1cc
NoRezap <norezap@proton.me>
2026-05-21 19:45:38 +0100

    Refactor common/location

--- src/common/location/mod.rs
+++ src/common/location/mod.rs
@@ -18,19 +18,18 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 use std::{
     fmt::Display,
     path::{Path, PathBuf},
-    sync::Arc,
 };
 
 use crate::{
     Db,
-    ril::{FileModule, ModuleId, Package},
+    ril::{FileModule, Package},
 };
 
 fn get_source_file_in_submodule<'db>(
     db: &'db dyn Db,
     file: impl AsRef<Path>,
     submodule: FileModule<'db>,
-) -> Option<SourceFile<'db>> {
+) -> Option<SourceFile> {
     if submodule.file(db).path(db).as_path() == file.as_ref() {
         return Some(submodule.file(db));
     }
@@ -46,7 +45,7 @@ fn get_source_file<'db>(
     db: &'db dyn Db,
     file: impl AsRef<Path>,
     packages: &[Package<'db>],
-) -> Option<SourceFile<'db>> {
+) -> Option<SourceFile> {
     for package in packages {
         let source_file = package.root(db).file(db);
         if source_file.path(db) == file.as_ref() {
@@ -63,83 +62,114 @@ fn get_source_file<'db>(
     None
 }
 
-#[salsa::interned]
-#[derive(PartialOrd, Ord)]
+#[salsa::input]
+#[derive(Debug)]
 pub struct SourceFile {
+    #[returns(ref)]
     pub path: PathBuf,
-    pub content: Arc<String>,
+    #[returns(ref)]
+    pub content: String,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct OwnedSourceFile {
-    pub path: PathBuf,
-    pub content: Arc<String>,
-}
-
-impl OwnedSourceFile {
-    pub fn new(path: PathBuf, content: Arc<String>) -> Self {
-        Self { path, content }
-    }
-
-    pub fn to_source_file<'db>(&self, db: &'db dyn Db) -> SourceFile<'db> {
-        SourceFile::new(db, self.path.clone(), self.content.clone())
-    }
-}
-
-impl<'db> SourceFile<'db> {
-    pub fn to_owned(&self, db: &'db dyn Db) -> OwnedSourceFile {
-        OwnedSourceFile::new(self.path(db), self.content(db))
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Location {
+    pub file: SourceFile,
     pub offset: usize,
-    pub file: PathBuf,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Span {
-    pub start: usize,
-    pub end: usize,
-    pub file: PathBuf,
+    pub file: SourceFile,
+    pub start_offset: usize,
+    pub end_offset: usize,
 }
 
 impl Span {
-    pub fn new(start: usize, end: usize, file: PathBuf) -> Self {
-        Self { start, end, file }
+    pub fn start(self) -> Location {
+        Location {
+            file: self.file,
+            offset: self.start_offset,
+        }
     }
 
-    pub fn start(&self) -> Location {
-        Location::new(self.start, self.file.clone())
+    pub fn end(self) -> Location {
+        Location {
+            file: self.file,
+            offset: self.end_offset,
+        }
     }
 
-    pub fn end(&self) -> Location {
-        Location::new(self.end, self.file.clone())
-    }
-}
-
-impl Location {
-    pub fn new(offset: usize, file: PathBuf) -> Self {
-        Self { offset, file }
-    }
-
-    pub fn advance(&self, offset: usize) -> Self {
+    pub fn new(file: SourceFile, start_offset: usize, end_offset: usize) -> Self {
         Self {
-            offset: self.offset + offset,
-            file: self.file.clone(),
+            file,
+            start_offset,
+            end_offset,
         }
     }
 }
 
 impl Location {
-    #[allow(dead_code)]
-    pub fn span(&self, other: &Self) -> Span {
-        assert!(self.file == other.file);
-        let start_offset = self.offset.min(other.offset);
-        let end_offset = self.offset.max(other.offset);
-        Span::new(start_offset, end_offset, self.file.clone())
+    pub fn advance(self, offset: usize) -> Self {
+        // TODO: maybe verify the validity
+        let mut this = self;
+        this.offset += offset;
+        this
     }
+
+    pub fn span(self, other: Self) -> Span {
+        let file = self.file;
+        if file != other.file {
+            panic!("Span across different files");
+        }
+        let (start_offset, end_offset) = if self.offset > other.offset {
+            (other.offset, self.offset)
+        } else {
+            (self.offset, other.offset)
+        };
+        Span {
+            file,
+            start_offset,
+            end_offset,
+        }
+    }
+
+    pub fn loc_info(self, db: &dyn Db) -> LocationInfo {
+        _loc_info(db, self)
+    }
+
+    pub fn new(file: SourceFile, offset: usize) -> Self {
+        Self { file, offset }
+    }
+}
+
+fn _loc_info<'db>(db: &'db dyn Db, loc: Location) -> LocationInfo {
+    #[salsa::interned]
+    struct Interned {
+        inner: Location,
+    }
+    #[salsa::tracked]
+    fn _tracked<'a>(db: &'a dyn Db, loc: Interned<'a>) -> LocationInfo {
+        let loc = loc.inner(db);
+        let mut line = 1;
+        let mut column = 1;
+        let offset = loc.offset;
+        let contents = &loc.file.content(db)[..offset];
+        for c in contents.chars() {
+            if c == '\n' {
+                line += 1;
+                column = 1;
+            } else {
+                column += 1;
+            }
+        }
+        LocationInfo {
+            file: loc.file.path(db).clone(),
+            line,
+            column,
+            offset,
+        }
+    }
+    _tracked(db, Interned::new(db, loc))
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -148,46 +178,6 @@ pub struct LocationInfo {
     pub line: usize,
     pub column: usize,
     pub offset: usize,
-}
-
-impl Location {
-    pub fn loc_info(&self, db: &dyn crate::Db, module: ModuleId) -> Option<LocationInfo> {
-        let package = module.owning_package(db)?;
-        let sf = get_source_file(db, self.file.as_path(), &[package])?;
-        Some(get_loc_info(db, sf, self.offset))
-    }
-}
-
-#[salsa::tracked]
-pub fn get_loc_info<'db>(
-    db: &'db dyn crate::Db,
-    file: SourceFile<'db>,
-    offset: usize,
-) -> LocationInfo {
-    compute_loc_info(file.content(db).as_ref(), offset, file.path(db))
-}
-
-pub fn compute_loc_info(s: &str, offset: usize, file: PathBuf) -> LocationInfo {
-    let mut line = 1;
-    let mut column = 1;
-    for i in 0..offset {
-        if let Some(c) = s[i..].chars().next() {
-            if c == '\n' {
-                line += 1;
-                column = 1;
-            } else {
-                column += 1;
-            }
-        } else {
-            break;
-        }
-    }
-    LocationInfo {
-        file,
-        line,
-        column,
-        offset,
-    }
 }
 
 impl Display for LocationInfo {
