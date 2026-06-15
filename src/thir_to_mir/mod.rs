c46f5d8f2fbc598de571f667290fcead80c00dcd
NoRezap <norezap@proton.me>
2026-06-15 22:34:53 +0100

    Started working on handling thir locals in thir -> MIR

--- src/thir_to_mir/mod.rs
+++ src/thir_to_mir/mod.rs
@@ -15,13 +15,13 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use crate::{
     Db,
-    mir::{MIR, builder::MIRBuilder},
-    ril::{FunctionId, TypeRef},
-    thir::{Thir, thir_body},
+    mir::{MIR, MIRLocal, MIRLocalID, SyntacticSource, builder::MIRBuilder},
+    ril::{FunctionId, TypeId, TypeRef},
+    thir::{self, Thir, thir_body},
 };
 
 pub struct ThirToMIR<'a> {
@@ -29,6 +29,9 @@ pub struct ThirToMIR<'a> {
     builder: MIRBuilder<'a>,
     thir: &'a Thir,
     subs: &'a [TypeRef],
+
+    local_map: HashMap<thir::LocalId, MIRLocalID>,
+    params: Vec<MIRLocalID>,
 }
 
 #[salsa::interned]
@@ -66,10 +69,99 @@ impl<'a> ThirToMIR<'a> {
             builder: MIRBuilder::new(db, Some("entry".into())),
             thir,
             subs,
+            local_map: HashMap::new(),
+            params: Vec::new(),
         }
     }
 
-    pub fn lower(self) -> MIR {
+    pub fn is_valid_ty(&self, ty: TypeRef) -> bool {
+        match ty {
+            TypeRef::Concrete(type_id) => type_id
+                .args(self.db)
+                .into_iter()
+                .all(|ty| self.is_valid_ty(ty)),
+            TypeRef::Param(_) => false,
+            TypeRef::Associated(_) => todo!(),
+            _ => false,
+        }
+    }
+
+    /// Returns the substituted form of the type
+    pub fn ty(&self, ty: TypeRef) -> TypeRef {
+        let res = match ty {
+            TypeRef::Concrete(type_id) => TypeRef::Concrete(TypeId::new(
+                self.db,
+                type_id.def(self.db),
+                type_id
+                    .args(self.db)
+                    .into_iter()
+                    .map(|ty| self.ty(ty))
+                    .collect(),
+            )),
+            TypeRef::Param(id) => self.subs[id.0],
+            TypeRef::Zelf => {
+                let unsub = self
+                    .thir
+                    .id
+                    .parent(self.db)
+                    .get_canonical_zelf(self.db)
+                    .unwrap_or(TypeRef::Error);
+                if matches!(unsub, TypeRef::Zelf) {
+                    // Do not recurse infinitely, bail early
+                    TypeRef::Error
+                } else {
+                    self.ty(unsub)
+                }
+            }
+            TypeRef::Associated(_) => todo!(),
+            _ => ty,
+        };
+        if !self.is_valid_ty(res) {
+            panic!("Not a valid ty")
+        }
+        res
+    }
+
+    fn build_thir_locals(&mut self) {
+        for (thir_id, local) in self.thir.locals.iter() {
+            let ty = self.ty(local.ty);
+            let mut mir = MIRLocal::new(ty, local.mutability, local.span);
+            if let Some(src) = &local.source {
+                mir = mir.with_name(src.1).with_thir_src(thir_id).with_syn_src(
+                    SyntacticSource {
+                        span: local.span,
+                        id: src.0,
+                    },
+                )
+            }
+            let mir_id = self.builder.new_local(mir);
+
+            self.local_map.insert(thir_id, mir_id);
+        }
+    }
+
+    fn build_arguments(&mut self) {
+        for param in &self.thir.params {
+            let mir_id = *self.local_map.get(param).expect("Expected all local thir ids to be found in self.local_map. Maybe try calling `build_thir_locals`.");
+            self.params.push(mir_id);
+        }
+    }
+
+    fn check_substitution(&self) {
+        for ty in self.subs {
+            if !self.is_valid_ty(*ty) {
+                panic!(
+                    "Expecting a valid type in substitution but got {}",
+                    ty.to_string(self.db)
+                )
+            }
+        }
+    }
+
+    pub fn lower(mut self) -> MIR {
+        self.check_substitution();
+        self.build_thir_locals();
+        self.build_arguments();
         todo!()
     }
 }
