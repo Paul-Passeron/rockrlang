97079e403fdd0c74e8c2a533d3b4c7daf48fd50e
NoRezap <norezap@proton.me>
2026-06-19 19:27:31 +0100

    Began working on liveness analysis

--- src/mir/analysis/liveness.rs
+++ src/mir/analysis/liveness.rs
@@ -14,3 +14,61 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+use std::collections::{HashMap, HashSet};
+
+use crate::{
+    Db,
+    mir::{MIR, MIRBlockID, MIRLocalID, analysis::MIRAnalysis},
+};
+
+pub struct MIRLivenessAnalysis;
+
+pub struct MIRLivenessResult {
+    pub live_in: HashMap<MIRBlockID, HashSet<MIRLocalID>>,
+    pub live_out: HashMap<MIRBlockID, HashSet<MIRLocalID>>,
+}
+
+impl MIRAnalysis for MIRLivenessAnalysis {
+    type Out = MIRLivenessResult;
+
+    fn run<'db, 'mir>(&self, db: &'db dyn Db, mir: &'mir MIR) -> Self::Out {
+        LivCtx::new(db, mir).run()
+    }
+}
+
+struct LivCtx<'a> {
+    db: &'a dyn Db,
+    mir: &'a MIR,
+    live_in: HashMap<MIRBlockID, HashSet<MIRLocalID>>,
+    live_out: HashMap<MIRBlockID, HashSet<MIRLocalID>>,
+}
+
+impl<'a> LivCtx<'a> {
+    pub fn new(db: &'a dyn Db, mir: &'a MIR) -> Self {
+        Self {
+            db,
+            mir,
+            live_in: Self::get_start_live(mir),
+            live_out: Self::get_start_live(mir),
+        }
+    }
+
+    pub fn run(mut self) -> MIRLivenessResult {
+        self.finalize()
+    }
+
+    fn finalize(self) -> MIRLivenessResult {
+        MIRLivenessResult {
+            live_in: self.live_in,
+            live_out: self.live_out,
+        }
+    }
+
+    fn get_start_live(mir: &MIR) -> HashMap<MIRBlockID, HashSet<MIRLocalID>> {
+        mir.blocks
+            .iter()
+            .map(|(id, _)| (id, HashSet::new()))
+            .collect()
+    }
+}
--- src/mir/analysis/mod.rs
+++ src/mir/analysis/mod.rs
@@ -15,4 +15,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+use crate::{Db, mir::MIR};
+
 pub mod liveness;
+
+pub trait MIRAnalysis {
+    type Out;
+
+    fn run<'db, 'mir>(&self, db: &'db dyn Db, mir: &'mir MIR) -> Self::Out;
+}
