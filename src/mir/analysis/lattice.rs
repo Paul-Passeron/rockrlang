caf5c3f0b5a51d9b109ffba5bacd0a4776d3084a
NoRezap <norezap@proton.me>
2026-06-19 22:35:54 +0100

    Implemented generic fixed-point iteration in MIR using lattices

--- src/mir/analysis/lattice.rs
+++ src/mir/analysis/lattice.rs
@@ -1,9 +1,23 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{
+    collections::{BTreeSet, HashMap},
+    hash::Hash,
+};
+
+use crate::mir::{MIR, MIRBlockID};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum LatticeChange {
     Changed,
     Unchanged,
+}
+
+impl From<bool> for LatticeChange {
+    fn from(changed: bool) -> Self {
+        match changed {
+            true => Self::Changed,
+            false => Self::Unchanged,
+        }
+    }
 }
 
 impl LatticeChange {
@@ -32,11 +46,7 @@ pub trait Lattice: Eq + Clone {
 
     fn join_assign(&mut self, other: &Self) -> LatticeChange {
         let joined = self.join(other);
-        let changed = if joined != *self {
-            LatticeChange::Changed
-        } else {
-            LatticeChange::Unchanged
-        };
+        let changed = (joined != *self).into();
         *self = joined;
         changed
     }
@@ -83,5 +93,83 @@ where
             }
         }
         change
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Direction {
+    Forward,
+    Backward,
+}
+
+pub type BlockMap<L> = HashMap<MIRBlockID, L>;
+
+pub struct FixedPointIterRes<L: Lattice> {
+    pub block_in: BlockMap<L>,
+    pub block_out: BlockMap<L>,
+}
+
+impl MIR {
+    pub fn get_block_bottoms<L: Lattice>(&self) -> impl Iterator<Item = (MIRBlockID, L)> {
+        self.blocks.keys().map(|blk| (blk, L::bottom()))
+    }
+
+    pub fn fixed_point_iter<L: Lattice>(
+        &self,
+        direction: Direction,
+        transfer: impl Fn(MIRBlockID, &L) -> L,
+        in_seed: Option<BlockMap<L>>,
+        out_seed: Option<BlockMap<L>>,
+    ) -> FixedPointIterRes<L> {
+        let mut block_in = BlockMap::from_iter(self.get_block_bottoms::<L>());
+        if let Some(in_seed) = in_seed {
+            block_in.join_assign(&in_seed);
+        }
+
+        let mut block_out = BlockMap::from_iter(self.get_block_bottoms::<L>());
+        if let Some(out_seed) = out_seed {
+            block_out.join_assign(&out_seed);
+        }
+
+        let succs = self.compute_successors();
+        let preds = self.compute_predecessors(&succs);
+
+        let mut worklist: BTreeSet<_> = BTreeSet::from_iter(self.blocks.keys());
+
+        while let Some(blk) = worklist.pop_last() {
+            let new_in = match direction {
+                Direction::Forward => preds
+                    .get(&blk)
+                    .into_iter()
+                    .flatten()
+                    .fold(L::bottom(), |l, p| l.join(&block_out[p])),
+                Direction::Backward => transfer(blk, &block_out[&blk]),
+            };
+            let new_out = match direction {
+                Direction::Forward => transfer(blk, &block_in[&blk]),
+                Direction::Backward => succs
+                    .get(&blk)
+                    .into_iter()
+                    .flatten()
+                    .fold(L::bottom(), |l, p| l.join(&block_in[p])),
+            };
+
+            let changed = block_in[&blk] != new_in || block_out[&blk] != new_out;
+
+            block_in.insert(blk, new_in);
+            block_out.insert(blk, new_out);
+
+            if changed {
+                match direction {
+                    Direction::Forward => worklist.extend(succs[&blk].iter().copied()),
+                    Direction::Backward => worklist.extend(preds[&blk].iter().copied()),
+                }
+            }
+        }
+
+        FixedPointIterRes {
+            block_in,
+            block_out,
+        }
     }
 }
