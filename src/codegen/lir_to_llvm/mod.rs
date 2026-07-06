90725d8fcc14db6eaf213a7fdadde8fad1a879d8
NoRezap <norezap@proton.me>
2026-07-06 20:29:16 +0200

    Start working on LIR -> LLVM

--- src/codegen/lir_to_llvm/mod.rs
+++ src/codegen/lir_to_llvm/mod.rs
@@ -15,13 +15,20 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-use inkwell::{builder::Builder, context::Context};
+use inkwell::{
+    builder::Builder,
+    context::Context,
+    module::Linkage,
+    types::{AnyTypeEnum, BasicMetadataTypeEnum, BasicType, BasicTypeEnum},
+    values::FunctionValue,
+};
+use itertools::Itertools;
 
 use crate::{
     Db,
     codegen::{Codegen, IModule, LIRToLLVM},
-    lir::{self, Complete, LIRFunctionId},
-    unused,
+    layout::LayoutID,
+    lir::{self, Complete, FunctionSig, LIRFunctionId},
 };
 
 impl<'db> Codegen<'db, LIRToLLVM<'db>> {
@@ -52,11 +59,66 @@ struct Ctx<'db, 'lir, 'ctx> {
 }
 
 impl<'db, 'lir, 'ctx> Ctx<'db, 'lir, 'ctx> {
-    fn run(self) {
+    fn run(mut self) {
+        self.lir.functions().for_each(|func| self.declare_lir(func));
         self.lir.functions().for_each(|func| self.lower_lir(func));
     }
 
-    fn lower_lir(&self, l: LIRFunctionId) {
-        unused!(l);
+    fn new_function_value(&mut self, sig: &FunctionSig) -> FunctionValue<'ctx> {
+        let params = sig
+            .signature
+            .params
+            .iter()
+            .flat_map(|layout| {
+                if layout.is_zst(self.db) {
+                    None
+                } else {
+                    Some(
+                        BasicMetadataTypeEnum::try_from(
+                            self.lower_layout(layout.layout),
+                        )
+                        .unwrap(),
+                    )
+                }
+            })
+            .collect_vec();
+        let fn_ty = if sig.signature.ret.is_zst(self.db) {
+            BasicTypeEnum::try_from(self.lower_layout(sig.signature.ret.layout))
+                .unwrap()
+                .fn_type(&params, false)
+        } else {
+            self.ctx.void_type().fn_type(&params, false)
+        };
+        self.m.add_function(
+            &sig.name,
+            fn_ty,
+            match sig.kind {
+                lir::SigKind::Import => None,
+                lir::SigKind::Defined(defined_linkage) => {
+                    Some(match defined_linkage {
+                        lir::DefinedLinkage::Export => Linkage::External,
+                        lir::DefinedLinkage::Local => Linkage::Private,
+                        lir::DefinedLinkage::Weak => Linkage::ExternalWeak,
+                    })
+                }
+            },
+        )
+    }
+
+    fn lower_layout(&mut self, layout: LayoutID) -> AnyTypeEnum<'ctx> {
+        todo!()
+    }
+
+    fn get_llvm_func(&self, l: LIRFunctionId) -> FunctionValue<'ctx> {
+        self.m.get_function(&self.lir.get_fn(l).0.name).unwrap()
+    }
+
+    fn declare_lir(&mut self, l: LIRFunctionId) {
+        let (sig, _) = self.lir.get_fn(l);
+        let new_fn_value = self.new_function_value(sig);
+    }
+
+    fn lower_lir(&mut self, l: LIRFunctionId) {
+        let (sig, body) = self.lir.get_fn(l);
     }
 }
